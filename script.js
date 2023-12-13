001bab0ac0f153cfb704dfd19c4f260c86e8eac4
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-12-13 12:14:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let trackingSum = 0, ans =[], subArr = [];
+
+	for(let i = 0;i < arr.length;i++){
+		if(trackingSum + arr[i] <= n){
+			subArr.push(arr[i]);
+			trackingSum += arr[i];
+		}
+		else{
+			ans.push(subArr);
+			//start a new subArray freshly with the current
+			subArr = [arr[i]];
+			trackingSum = arr[i];
+		}
+	}
+	ans.push(subArr);
+	return ans;
 };
 
 const n = prompt("Enter n: ");
